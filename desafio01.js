1341f0bfc9f72dfe5ce01ce232526f497d5f523b
edsonssoares <edsonssoares@gmail.com>
2022-06-27 22:31:12 -0300

    first commit

---
+++ desafio01.js
@@ -0,0 +1,10 @@
+"use strict";
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+/* let employee = {};
+employee.code = 10;
+employee.name = "John"; */
+// Resposta
+const Funcionario = {
+    codigo: 10,
+    nome: 'joao'
+};
